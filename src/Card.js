9b27c1491d7963b8511d849e6ab23eb0565503ce
Cara Steffen <cara@treadstonedesign.com>
2019-08-18 15:55:40 -0600

    initial commit

---
+++ src/Card.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Card.css";
+
+/*
+The Card component should render markup matching the design.html: 
+    a div with a class of 'Card' containing an h3 for the card's title and 
+    a p element for the card's content.
+
+The Card component accepts 2 props: title and content.
+    title is a string of the card's title.
+    content is a string of the card's content.
+    These props will be passed in for each Card from the List component.
+*/
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="Card">
+        <button type="button">delete</button>
+        <h3>{this.props.title}</h3>
+        <p>{this.props.content}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
